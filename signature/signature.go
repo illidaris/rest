23a406874bf5782171f8d4432d202fac52dfd39b
Joseph Wang <Ivan_Whisper@outlook.com>
2024-01-19 16:49:09 +0800

    Update signature.go

--- signature/signature.go
+++ signature/signature.go
@@ -15,22 +15,38 @@ type Signature interface {
 	ToMap() url.Values
 }
 
-func NewSignatureFrmRequest(req *http.Request) Signature {
-	signFrmRquest := &DefaultSignature{} // sign data from request
+func NewSignatureFrmRequest(req *http.Request) (Signature, url.Values) {
+	var (
+		signFrmRquest = &DefaultSignature{} // sign data from request
+		otherValues   = url.Values{}
+	)
 	urlVs := req.URL.Query()
+	for k, v := range req.URL.Query() {
+		switch k {
+		case SignKeySign, SignToken:
+			continue
+		default:
+			otherValues[k] = v
+			continue
+		}
+	}
 	if signFrmRquest.Sign = urlVs.Get(SignKeySign); signFrmRquest.Sign == "" {
 		signFrmRquest.Sign = req.Header.Get(SignKeySign)
 	}
 	if signFrmRquest.Timestamp = cast.ToInt64(urlVs.Get(SignKeyTimestamp)); signFrmRquest.Timestamp == 0 {
-		signFrmRquest.Timestamp = cast.ToInt64(req.Header.Get(SignKeyTimestamp))
+		v := req.Header.Get(SignKeyTimestamp)
+		signFrmRquest.Timestamp = cast.ToInt64(v)
+		otherValues[SignKeyTimestamp] = []string{v}
 	}
 	if signFrmRquest.Noise = urlVs.Get(SignKeyNoise); signFrmRquest.Noise == "" {
 		signFrmRquest.Noise = req.Header.Get(SignKeyNoise)
+		otherValues[SignKeyNoise] = []string{signFrmRquest.Noise}
 	}
 	if signFrmRquest.AppID = urlVs.Get(SignAppID); signFrmRquest.AppID == "" {
 		signFrmRquest.AppID = req.Header.Get(SignAppID)
+		otherValues[SignAppID] = []string{signFrmRquest.AppID}
 	}
-	return signFrmRquest
+	return signFrmRquest, otherValues
 }
 
 type DefaultSignature struct {
