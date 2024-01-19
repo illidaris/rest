63936642da99ee376cd5ee182373df6e863001a0
Joseph Wang <Ivan_Whisper@outlook.com>
2024-01-19 16:51:41 +0800

    Update verify.go

--- signature/verify.go
+++ signature/verify.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/illidaris/rest/core"
-	"github.com/spf13/cast"
 )
 
 func VerifySign(req *http.Request, opts ...OptionFunc) error {
@@ -19,11 +18,11 @@ func VerifySign(req *http.Request, opts ...OptionFunc) error {
 	}
 	contentType := req.Header.Get("Content-Type")
 	// take sign data from request
-	signFrmRquest := NewSignatureFrmRequest(req) // sign data from request
+	signFrmRquest, params := NewSignatureFrmRequest(req)  // sign data from request
 	if err := signOpt.Valid(signFrmRquest.GetTimestamp()); err != nil {
 		return err
 	}
-	params := url.Values{} // params will be hash to sgin
+	// params := url.Values{} // params will be hash to sgin
 	// form
 	if req.Method == http.MethodGet || req.Method == http.MethodDelete || contentType == core.FormUrlEncode.ToCode() {
 		_ = req.ParseForm()
@@ -42,9 +41,9 @@ func VerifySign(req *http.Request, opts ...OptionFunc) error {
 		req.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 		params.Add(SignBody, string(bodyBytes))
 	}
-	params.Add(SignKeyTimestamp, cast.ToString(signFrmRquest.GetTimestamp()))
-	params.Add(SignKeyNoise, signFrmRquest.GetNoise())
-	params.Add(SignAppID, signFrmRquest.GetAppID())
+	// params.Add(SignKeyTimestamp, cast.ToString(signFrmRquest.GetTimestamp()))
+	// params.Add(SignKeyNoise, signFrmRquest.GetNoise())
+	// params.Add(SignAppID, signFrmRquest.GetAppID())
 	// format data
 	action := req.URL.Path
 	// /api/test => api/test
