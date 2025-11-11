12c5243f83ddb839a235f9fa8351317d2c83d0f2
Joseph Wang <ivan_whisper@outlook.com>
2025-11-11 17:42:04 +0800

    ignore

--- log/log.go
+++ log/log.go
@@ -5,6 +5,31 @@ import (
 	"fmt"
 )
 
+type CtxKeyIgnore struct{}
+
+var ctxKeyIgnore CtxKeyIgnore
+
+// 根据上下文，不打日志
+func WithRestLogIgnore(ctx context.Context) context.Context {
+	v := ctx.Value(ctxKeyIgnore)
+	if v != nil {
+		return ctx
+	}
+	return context.WithValue(ctx, ctxKeyIgnore, true)
+}
+
+func RestLogIgnoreFrmCtx(ctx context.Context) bool {
+	v := ctx.Value(ctxKeyIgnore)
+	if v == nil {
+		return false
+	}
+	b, ok := v.(bool)
+	if !ok {
+		return false
+	}
+	return b
+}
+
 var _ = ILogger(&DefaultLogger{})
 
 type ILogger interface {
@@ -17,18 +42,30 @@ type ILogger interface {
 type DefaultLogger struct {
 }
 
-func (l *DefaultLogger) DebugCtx(_ context.Context, msg string) {
+func (l *DefaultLogger) DebugCtx(ctx context.Context, msg string) {
+	if RestLogIgnoreFrmCtx(ctx) {
+		return
+	}
 	fmt.Println(msg)
 }
 
-func (l *DefaultLogger) InfoCtx(_ context.Context, msg string) {
+func (l *DefaultLogger) InfoCtx(ctx context.Context, msg string) {
+	if RestLogIgnoreFrmCtx(ctx) {
+		return
+	}
 	fmt.Println(msg)
 }
 
-func (l *DefaultLogger) WarnCtx(_ context.Context, msg string) {
+func (l *DefaultLogger) WarnCtx(ctx context.Context, msg string) {
+	if RestLogIgnoreFrmCtx(ctx) {
+		return
+	}
 	fmt.Println(msg)
 }
 
-func (l *DefaultLogger) ErrorCtx(_ context.Context, msg string) {
+func (l *DefaultLogger) ErrorCtx(ctx context.Context, msg string) {
+	if RestLogIgnoreFrmCtx(ctx) {
+		return
+	}
 	fmt.Println(msg)
 }
